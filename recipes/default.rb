26dbb69524ed6bee6317c0fea8879777952161a9
Panggi Libersa Jasri Akadol <panggilja@gmail.com>
2014-06-13 10:59:45 +0700

    remove nodejs and add sqlite + memcached

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,10 +4,5 @@ package 'libxml2-devel'
 package 'libxslt'
 package 'libxslt-devel'
 package 'mysql-devel'
-execute "setting up nodejs repo" do
-  user 'root'
-  command 'rpm --import https://fedoraproject.org/static/0608B895.txt'
-  command 'rpm -Uvh http://download-i2.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm'
-  command 'yum install nodejs npm --enablerepo=epel'
-  action :run
-end
+package 'sqlite3-devel'
+package 'memcached'
