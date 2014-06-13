c6e839aacf38f861fd22ff1541f67ad03168b98d
Panggi Libersa Jasri Akadol <panggilja@gmail.com>
2014-06-13 11:04:42 +0700

    v0.0.3

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "rails_gem_dependencies-centos"
 maintainer        "Panggi Libersa Jasri Akadol"
 maintainer_email  "panggi@me.com"
 description       "Installs packages commonly required by rails gems"
-version           "0.0.2"
+version           "0.0.3"
 
 recipe "rails_gem_dependencies-centos", "gem dependencies for Centos"
 
